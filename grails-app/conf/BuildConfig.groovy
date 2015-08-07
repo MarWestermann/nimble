553a7f15f9c49b52fff3ce6e7559868fb2ebe5ea
Marco Westermann <mw@intersales.de>
2015-08-07 23:09:19 +0200

    make nimble compilable for grails 2.5.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,14 +1,19 @@
 grails.project.work.dir = 'target'
 
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
 
 	inherits 'global'
 	log 'warn'
 
 	repositories {
-		grailsCentral()
-		mavenLocal()
-		mavenCentral()
+		inherits true
+		
+		grailsPlugins()
+        grailsHome()
+        mavenLocal()
+        grailsCentral()
+        mavenCentral()
 	}
 
 	dependencies {
@@ -17,16 +22,17 @@ grails.project.dependency.resolution = {
 
 	plugins {
 		runtime ":twitter-bootstrap:2.3.0"
-		runtime ":resources:1.2"
-		runtime ":fields:1.2"
-		runtime ":jquery:1.9.0"
-		compile (":shiro:1.2.0") {
+		runtime ":resources:1.2.14"
+		runtime ":fields:1.5.1"
+		runtime ":jquery:1.11.1"
+		compile (":shiro:1.2.1") {
 			excludes "servlet-api"
 		}
-		compile ":mail:1.0.1"
+		compile ":mail:1.0.7"
 
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
-			export = false
-		}
+		
 	}
 }
+
+grails.project.repos.default = "buster"
+release.scm.enabled = false
